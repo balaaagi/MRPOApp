7fe9dd8aa5eac3a8bc2bede47bff6e00008d9184
Balaji S <balaaagi@gmail.com>
2016-01-09 21:53:54 +0530

    Received the Intent From Main Activity in Detail Activity

--- app/src/main/java/sunshine/balaaagi/me/sunshine/DetailActivityFragment.java
+++ app/src/main/java/sunshine/balaaagi/me/sunshine/DetailActivityFragment.java
@@ -1,10 +1,12 @@
 package sunshine.balaaagi.me.sunshine;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +19,12 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        Intent intent=getActivity().getIntent();
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        if(intent!=null && intent.hasExtra(intent.EXTRA_TEXT)){
+            String foreCastString=intent.getStringExtra(intent.EXTRA_TEXT);
+            ((TextView)rootView.findViewById(R.id.forecast_details)).setText(foreCastString);
+        }
+        return rootView;
     }
 }
